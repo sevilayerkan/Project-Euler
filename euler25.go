2a8493e8dcaaefbf1b6bd52f700156ef288b937b
Sevilay Erkan <sevilayerkan2@gmail.com>
2022-08-16 18:10:12 +0300

    Add new empty projects

---
+++ euler25.go
@@ -0,0 +1,21 @@
+//Author: @sevilayerkan
+//Date: 16.08.2022
+
+/* Problem case: What is the index of the first term in the Fibonacci sequence to contain 1000 digits? */
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	fmt.Println("dsfgfd")
+
+}
+
+func fibonacci(num int) int {
+
+	return num
+}
